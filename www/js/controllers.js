245ef1c7b91e9919fed259f90f7aa74019d74b85
giles-t <giles.tamplin@gmail.com>
2014-04-08 10:57:01 -0700

    Linted

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,13 +2,14 @@
 
 /* Controllers */
 
-app.controller('mediaListController', function ($scope, $stateParams, userService, mediaListService, mediaService, modalService, activeUserService) {
+app.controller('mediaListController', function ($scope, $stateParams, $timeout, userService, mediaListService, mediaService, modalService, activeUserService) {
 
-	console.log('List Controller');
+  console.log('List Controller');
 
   var users = activeUserService;
 
-  setTimeout( function () {
+  // Let the transition finish before loading content
+  $timeout( function () {
     $scope.user = users[$stateParams.user];
     console.log($scope.user);
     userService.async($scope.user.instagramID).then(function(res) {
@@ -21,59 +22,59 @@ app.controller('mediaListController', function ($scope, $stateParams, userServic
     });
   }, 500);
 
-	$scope.$on('$stateChangeSuccess', function (scope, args) {
-		if (args.name === 'user') {
+  $scope.$on('$stateChangeSuccess', function (scope, args) {
+    if (args.name === 'user') {
       $('#modal').modal('hide');
       $('body').removeClass('modal-open');
       console.log('Close Modal');
-			modalService.set(null);
-		}
-	});
+      modalService.set(null);
+    }
+  });
 
 });
 
-app.controller('mediaController', function ($scope, $stateParams, $state, mediaService, modalService) {
+app.controller('mediaController', function ($scope, $stateParams, $state, $timeout, mediaService, modalService) {
 
-	console.log('Media Controller');
+  console.log('Media Controller');
 
-	$('#modal').modal('show');
+  $('#modal').modal('show');
 
-	var media = modalService.get();
+  var media = modalService.get();
 
-	if (media) {
-		$scope.media = media;
-	} else {
-		mediaService.async($stateParams.media).then(function(res) {
-			$scope.media = res.data;
-		});
-	}
+  if (media) {
+    $scope.media = media;
+  } else {
+    mediaService.async($stateParams.media).then(function(res) {
+      $scope.media = res.data;
+    });
+  }
 
-	$scope.closeModal = function (e) {
-		if ( $(e.target).is('#modal') || $(e.target).is('#modal_close') ) {
-			$('#modal').modal('hide');
-			// Let the modal transition finish
-			setTimeout( function () {
-				$state.go('^');
-			}, 400);
-		}
-	}
+  $scope.closeModal = function (e) {
+    if ( $(e.target).is('#modal') || $(e.target).is('#modal_close') ) {
+      $('#modal').modal('hide');
+      // Let the modal transition finish
+      $timeout( function () {
+        $state.go('^');
+      }, 400);
+    }
+  }
 
 });
 
 app.controller('contentController', function ($scope) {
-	$scope.faded = false;
+  $scope.faded = false;
 });
 
 app.controller('addController', function ($scope, pendingPeopleService, categoryService) {
 
-	$scope.success = false;
+  $scope.success = false;
   $scope.pendingPeople = pendingPeopleService;
   $scope.categories = categoryService;
 
-	console.log('Add Controller');
-	$scope.update = function(person) {
-		var added = angular.copy(person);
-		added.id = person.name.first.toLowerCase() +'_'+ person.name.last.toLowerCase();
+  console.log('Add Controller');
+  $scope.update = function(person) {
+    var added = angular.copy(person);
+    added.id = person.name.first.toLowerCase() +'_'+ person.name.last.toLowerCase();
     $scope.pendingPeople.$add(added);
     $scope.success = true;
   };
@@ -82,7 +83,7 @@ app.controller('addController', function ($scope, pendingPeopleService, category
     return angular.equals(person, $scope.pendingPeople);
   };
 
- 	$scope.successMessage = 'Thanks for contributing to follow.la, your submission will be reviewed and added shortly.';
+  $scope.successMessage = 'Thanks for contributing to follow.la, your submission will be reviewed and added shortly.';
 
 });
 
@@ -93,56 +94,56 @@ app.controller('adminController', function ($scope, activeUserService, pendingPe
   $scope.categories = categoryService;
 
   $scope.approve = function (key, person) {
-		$scope.activePeople[person.id] = person;
-		$scope.activePeople[person.id].$priority = parseFloat(person.category);
-		$scope.activePeople.$save(person.id);
-  	$scope.pendingPeople.$remove(key);
+    $scope.activePeople[person.id] = person;
+    $scope.activePeople[person.id].$priority = parseFloat(person.category);
+    $scope.activePeople.$save(person.id);
+    $scope.pendingPeople.$remove(key);
   };
 
   $scope.delete = function (key) {
-  	$scope.pendingPeople.$remove(key);
+    $scope.pendingPeople.$remove(key);
   };
 
-	$scope.personAlreadyExists = function(person) {
+  $scope.personAlreadyExists = function(person) {
     return $scope.activePeople[person.name.first + '_' + person.name.last];
   };
 
   $scope.categoryAlreadyExists = function (category) {
-  	return $scope.categories[category.name.toLowerCase()];
+    return $scope.categories[category.name.toLowerCase()];
   };
 
-	$scope.addCategory = function (category) {
-		var added = angular.copy(category);
-		if ($scope.categories.$getIndex().length) {
-			added.id = $scope.categories.$getIndex().length;
-		} else {added.id = 0}
-   	var cat = $scope.categories.$child(added.id);
-   	cat.$set(added);
-   	$scope.category.name = '';
+  $scope.addCategory = function (category) {
+    var added = angular.copy(category);
+    if ($scope.categories.$getIndex().length) {
+      added.id = $scope.categories.$getIndex().length;
+    } else {added.id = 0}
+    var cat = $scope.categories.$child(added.id);
+    cat.$set(added);
+    $scope.category.name = '';
   };
 
   $scope.removeCategory = function (category) {
-  	$scope.categories.$remove(category.id);
+    $scope.categories.$remove(category.id);
   };
 
   $scope.editingCat = false;
   $scope.editCategory = function (id) {
-  	$scope.editingCat = true;
+    $scope.editingCat = true;
   };
 
   $scope.doneEditCategory = function (id) {
-  	$scope.categories.$save(id);
-  	$scope.editingCat = false;
+    $scope.categories.$save(id);
+    $scope.editingCat = false;
   };
 
   $scope.editingActivePerson = false;
   $scope.editActivePerson = function (key) {
-  	$scope.editingActivePerson = key;
+    $scope.editingActivePerson = key;
   };
 
   $scope.doneEditActivePerson = function (id) {
-  	$scope.activePeople.$save(id);
-  	$scope.editingActivePerson = false;
+    $scope.activePeople.$save(id);
+    $scope.editingActivePerson = false;
   };
 
 });
